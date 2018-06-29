0ce31ea3aa523cc4e49b2e97fcf21b04d0ab3367
wangsiqi <385691045@qq.com>
2018-06-29 15:39:54 +0800

    other(page)： 自动生成模板修改

--- src/pages/home-pages/index/index.js
+++ src/pages/home-pages/index/index.js
@@ -11,4 +11,13 @@ export default class extends MyPage {
   async onLoad(options) {
     console.log(await wxp.getUserInfo())
   }
+
+    /**
+   * 初始化页面
+   * 重写父类的 _init()
+   * 异步调用栈 onLoad -> _origin -> _init
+   */
+  async _init(){
+
+  }
 }
--- src/pages/user-pages/my/my.js
+++ src/pages/user-pages/my/my.js
@@ -1,6 +1,7 @@
 // 此文件是由模板文件 ".dtpl/page/$rawModuleName.js.dtpl" 生成的，你可以自行修改模板
 
 import {pagify, MyPage, wxp} from 'base/'
+import { toast, modal } from 'utils/layer';
 
 @pagify()
 export default class extends MyPage {
@@ -9,6 +10,13 @@ export default class extends MyPage {
   }
 
   async onLoad(options) {
-    console.log(await wxp.getUserInfo())
+
+  }
+
+
+  changeName(){
+     wxp.switchTab({
+       url:'../../code-pages/code/code'
+     })
   }
 }
