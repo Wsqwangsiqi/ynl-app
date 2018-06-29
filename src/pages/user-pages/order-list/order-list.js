f6a734b7dd912c30746e13d5ea3c6c934467e325
wangsiqi <385691045@qq.com>
2018-06-29 15:39:11 +0800

    init(pages): 我的订单

---
+++ src/pages/user-pages/order-list/order-list.js
@@ -0,0 +1,23 @@
+// 此文件是由模板文件 ".dtpl/page/$rawModuleName.js.dtpl" 生成的，你可以自行修改模板
+
+import {pagify, MyPage, wxp} from 'base/'
+
+@pagify()
+export default class extends MyPage {
+  data = {
+
+  }
+
+  async onLoad(options) {
+    console.log(await wxp.getUserInfo())
+  }
+
+    /**
+   * 初始化页面
+   * 重写父类的 _init()
+   * 异步调用栈 onLoad -> _origin -> _init
+   */
+  async _init(){
+
+  }
+}
