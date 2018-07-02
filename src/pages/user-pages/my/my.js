dac9a2408d8965832931e24b8b5030dd9d9e49cc
wangsiqi <385691045@qq.com>
2018-07-02 10:25:17 +0800

    feat:(pages): 个人中心拨打电话

--- src/pages/user-pages/my/my.js
+++ src/pages/user-pages/my/my.js
@@ -1,8 +1,8 @@
 // 此文件是由模板文件 ".dtpl/page/$rawModuleName.js.dtpl" 生成的，你可以自行修改模板
 
 import { pagify, MyPage, wxp } from 'base/'
-import { toast, modal } from 'utils/layer';
-import { getPageConfig } from 'utils/helper';
+import { toast, modal } from 'utils/layer'
+import { getPageConfig } from 'utils/helper'
 
 @pagify()
 export default class extends MyPage {
@@ -15,9 +15,7 @@ export default class extends MyPage {
     }
   }
 
-  async onLoad(options) {
-
-  }
+  async onLoad(options) {}
 
   toOrder(e) {
     switch (e.currentTarget.dataset.type) {
@@ -25,15 +23,19 @@ export default class extends MyPage {
         wxp.navigateTo({
           url: `${getPageConfig('user/order').url}`
         })
-        break;
+        break
       case 'charging':
         wxp.navigateTo({
           url: `${getPageConfig('user/charging').url}`
         })
-        break;
+        break
+      case 'call':
+        const phone = e.currentTarget.dataset.phone
+        wxp.makePhoneCall({ phoneNumber: phone })
+        break
 
       default:
-        break;
+        break
     }
   }
 }
